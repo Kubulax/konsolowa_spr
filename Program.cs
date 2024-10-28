a488e2936f4ffc246369eda65389097627fd8204
Jakub_Koza_spr <kubulax25@gmail.com>
2024-10-28 12:18:43 +0100

    Dodano klasę

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace konsolowa
 {
+    public class MusicAlbum
+    {
+        public string artist;
+        public string album;
+        public int songsNumber;
+        public string year;
+        public int downloadNumber;
+
+        public MusicAlbum()
+        {
+
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
